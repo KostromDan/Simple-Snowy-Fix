feb30e15030606229cc52e6c5c891bb0fe68e819
KostromDan <kostromdan@gmail.com>
2025-03-02 05:06:43 +0300

    Started NeoForge port

---
+++ neoforge/src/main/java/dev/kostromdan/mods/simple_snowy_fix/neoforge/SimpleSnowyFixModNeoForge.java
@@ -0,0 +1,12 @@
+package dev.kostromdan.mods.simple_snowy_fix.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import dev.kostromdan.mods.simple_snowy_fix.SimpleSnowyFixMod;
+
+@Mod(SimpleSnowyFixMod.MOD_ID)
+public final class SimpleSnowyFixModNeoForge {
+    public SimpleSnowyFixModNeoForge() {
+        SimpleSnowyFixMod.init();
+    }
+}
