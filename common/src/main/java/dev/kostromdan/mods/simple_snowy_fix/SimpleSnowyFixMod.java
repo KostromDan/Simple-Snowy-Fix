723a35f7d25d1aa77a15682d0bc52ea0b15efec1
KostromDan <kostromdan@gmail.com>
2025-03-02 16:51:51 +0300

    RU compat
    (cherry picked from commit a9087827bc9b377259d9a24b9d38098d664c9cdb)

--- common/src/main/java/dev/kostromdan/mods/simple_snowy_fix/SimpleSnowyFixMod.java
+++ common/src/main/java/dev/kostromdan/mods/simple_snowy_fix/SimpleSnowyFixMod.java
@@ -1,8 +1,23 @@
 package dev.kostromdan.mods.simple_snowy_fix;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Blocks;
+
 public final class SimpleSnowyFixMod {
     public static final String MOD_ID = "simple_snowy_fix";
 
     public static void init() {
+    }
+
+    public static void placeSnowOnLeaves(LevelAccessor level, BlockPos leafPos) {
+        BlockPos snowPos = leafPos.above();
+        BlockPos upSnowPos1 = snowPos.above();
+        BlockPos upSnowPos2 = upSnowPos1.above();
+        BlockPos upSnowPos3 = upSnowPos2.above();
+
+        if (level.isEmptyBlock(snowPos) && level.getBiome(snowPos).value().shouldSnow(level, snowPos) && level.isEmptyBlock(upSnowPos1) && level.isEmptyBlock(upSnowPos2) && level.isEmptyBlock(upSnowPos3)) {
+            level.setBlock(snowPos, Blocks.SNOW.defaultBlockState(), 19);
+        }
     }
 }
