5e70523aedc2626c2600e415dcc17741437b2780
KostromDan <kostromdan@gmail.com>
2025-03-06 03:07:31 +0300

    Fix snow was generated on grown trees.

--- common/src/main/java/dev/kostromdan/mods/simple_snowy_fix/SimpleSnowyFixMod.java
+++ common/src/main/java/dev/kostromdan/mods/simple_snowy_fix/SimpleSnowyFixMod.java
@@ -1,6 +1,7 @@
 package dev.kostromdan.mods.simple_snowy_fix;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.Heightmap;
@@ -17,8 +18,10 @@ public final class SimpleSnowyFixMod {
 
     public static void placeSnowOnLeaves(LevelAccessor level, BlockPos leafPos) {
         BlockPos snowPos = leafPos.above();
-        if ((config.getPlaceSnowOnLeavesUnderLeaves() || level.getHeight(Heightmap.Types.MOTION_BLOCKING, snowPos.getX(), snowPos.getZ()) == snowPos.getY()) &&
+        if (level instanceof WorldGenRegion &&
+                (config.getPlaceSnowOnLeavesUnderLeaves() || level.getHeight(Heightmap.Types.MOTION_BLOCKING, snowPos.getX(), snowPos.getZ()) == snowPos.getY()) &&
                 level.isEmptyBlock(snowPos) &&
+                !level.isEmptyBlock(leafPos) &&
                 level.getBiome(snowPos).value().shouldSnow(level, snowPos)) {
             level.setBlock(snowPos, Blocks.SNOW.defaultBlockState(), 19);
         }
